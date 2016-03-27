b1f731308a5f5440af3120a0e87af446839e16f6
Nick S <nicksolano96@gmail.com>
2016-03-27 00:59:18 -0700

    Code Examples

---
+++ CodeExamples/DrawingFlagNS.java
@@ -0,0 +1,112 @@
+/**
+ * Created by NickS on 1/24/2016.
+ */
+
+//WOOP
+public class DrawingFlagNS {
+
+    public static void main(String args[]) {
+        System.out.println(" This is the Japaneese Flag!!");
+        for (int a = 1; a <= 30; a++) {
+            if (a % 2 == 0) {
+                System.out.print("_");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int a = 1; a <= 31; a++) {
+            if (a == 1 || a == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+
+            }
+        }
+
+        System.out.print("\n");
+
+        for (int i = 1; i <= 31; i++) {
+            if (i == 14 || i == 16 || i == 18) {
+                System.out.print("o");
+            } else if (i == 1 || i == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int i = 1; i <= 31; i++) {
+            if (i == 12 || i == 20) {
+                System.out.print("o");
+            } else if (i == 1 || i == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int i = 1; i <= 31; i++) {
+            if (i == 11 || i == 21) {
+                System.out.print("o");
+            } else if (i == 1 || i == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int i = 1; i <= 31; i++) {
+            if (i == 11 || i == 21) {
+                System.out.print("o");
+            } else if (i == 1 || i == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int i = 1; i <= 31; i++) {
+            if (i == 12 || i == 20) {
+                System.out.print("o");
+            } else if (i == 1 || i == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int i = 1; i <= 31; i++) {
+            if (i == 14 || i == 16 || i == 18) {
+                System.out.print("o");
+            } else if (i == 1 || i == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+        System.out.print("\n");
+        for (int a = 1; a <= 31; a++) {
+            if (a % 2 == 0) {
+                System.out.print("_");
+            } else if (a == 1 || a == 31) {
+                System.out.print("|");
+            } else {
+                System.out.print(" ");
+
+            }
+        }
+        System.out.print("\n");
+        System.out.print("ARIGATOU!");
+
+    }
+}
+
+
